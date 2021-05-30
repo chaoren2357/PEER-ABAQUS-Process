303a4c0b50dee40c659a51dfe66de6db305aa7c7
Zhiyuan Gao <839424923@qq.com>
2021-05-30 22:49:45 +0800

    Add project and other features

--- autoRun.py
+++ autoRun.py
@@ -19,73 +19,82 @@ import time
 
 def main():
 
-	## check exist res_dict
-	id_exist = []
-	for _,_,files in os.walk("Result"):
-		for filename in files:
-			if filename[:8] == "res_dict":
-				id_exist.append(filename[9:-4])
+	projectNames = ['ThreeLayersFrame','FiveLayersFrame']
+	for projectName in projectNames:
 
-	## log file init
-	with open("Result/log.txt",'a') as f:
-		f.write("Start running at {}\n".format(time.asctime( time.localtime(time.time()) )))
+		result_project_path = os.path.join("Result",projectName)
+		log_project_path = os.path.join("Result",projectName,"log.txt")
+		models_project_path = os.path.join("Models",projectName)
+
+		## check exist res_dict
+		id_exist = []
+		for _,_,files in os.walk(result_project_path):
+			for filename in files:
+				if filename[:8] == "res_dict":
+					id_exist.append(filename[9:-4])
+
+		## log file init
+		with open(log_project_path,'a') as f:
+			f.write("Start running at {}\n".format(time.asctime( time.localtime(time.time()) )))
 
 
-	## find inp file id
-	ids = []
-	for _,_,files in os.walk('Models'):
-		for filename in files:
-			if filename[:5] == "model" and filename != 'model_sample.inp':
-				ids.append(filename[5:-4])
-	
+		## find inp file id
+		ids = []
+		for _,_,files in os.walk(models_project_path):
+			for filename in files:
+				if filename[:5] == "model" and filename != 'model_sample.inp':
+					ids.append(filename[5:-4])
+		
 
-	# iterate from all ids
-	for waveid in ids:
+		# iterate from all ids
+		for waveid in ids:
 
-		if waveid in id_exist:
-			continue
+			if waveid in id_exist:
+				continue
 
-		s = time.time()
+			s = time.time()
 
-		# create name for model and job
-		modelName = 'model'+ str(waveid).zfill(3)
-		jobName = 'job'+ str(waveid).zfill(3)
+			# create name for model and job
+			modelName = 'model'+ str(waveid).zfill(3)
+			jobName = 'job'+ str(waveid).zfill(3)
+			model_path = os.path.join("Models",projectName,modelName)
+			pkl_path = os.path.join("Result","res_dict_{}.pkl".format(waveid))
 
-		myModel = mdb.ModelFromInputFile("ThreeLayerFrame", "Models/"+modelName+".inp")
-		myJob = mdb.Job(name=jobName, model='ThreeLayerFrame',description='Three Layer Frame seismic design')
+			myModel = mdb.ModelFromInputFile(projectName, model_path)
+			myJob = mdb.Job(name=jobName, model=projectName,description='{} seismic design'.format(projectName))
 
-		# Wait for the job to complete.
-		myJob.submit()
-		myJob.waitForCompletion()
+			# Wait for the job to complete.
+			myJob.submit()
+			myJob.waitForCompletion()
 
-		# open result odb file
-		odb = openOdb(path=jobName + '.odb')
-		for stepName in odb.steps.keys():
-			step = odb.steps[stepName]
-			for regionName in step.historyRegions.keys():
-				region = step.historyRegions[regionName]
-				for pointName in region.historyOutputs.keys():
-					data = region.historyOutputs[pointName].data
-					key = "{}@{}@{}@{}".format(waveid,stepName,regionName,pointName)
+			# open result odb file
+			res_dict = {}
+			odb = openOdb(path=jobName + '.odb')
+			for stepName in odb.steps.keys():
+				step = odb.steps[stepName]
+				for regionName in step.historyRegions.keys():
+					region = step.historyRegions[regionName]
+					for pointName in region.historyOutputs.keys():
+						data = region.historyOutputs[pointName].data
+						key = "{}@{}@{}@{}".format(waveid,stepName,regionName,pointName)
 
-					# add data into res_dict
-					if key not in res_dict:
+						# add data into res_dict
 						res_dict[key] = data
 
-		save_pkl(res_dict,"Result/res_dict_{}.pkl".format(waveid))
+			save_pkl(res_dict,pkl_path)
 
-		# log
-		e = time.time()
-		ht,mt,st = get_format_time(s,e)
-		with open("Result/log.txt",'a') as f:
-			f.write("Finish {},using time {}:{}:{}\n".format(modelName,ht,mt,st))
+			# log
+			e = time.time()
+			ht,mt,st = get_format_time(s,e)
+			with open(log_project_path,'a') as f:
+				f.write("Finish {},using time {}:{}:{}\n".format(modelName,ht,mt,st))
 
-		## kill the job and close odb
-		myJob.kill()
-		odb.close()
+			## kill the job and close odb
+			myJob.kill()
+			odb.close()
 
-	with open("Result/log.txt",'a') as f:
-		f.write("Finish all.............................................\n")
+		with open(log_project_path,'a') as f:
+			f.write("Finish all.............................................\n")
 
 if __name__ == '__main__':
 	main()
--- preprocess.py
+++ preprocess.py
@@ -10,8 +10,12 @@ generate modelXXX.inp to be the input file of abaqus
 
 import os
 from utils import *
+from pathlib import Path
 
-def create_model_inp(idx,seismic_data,addr = "Models"):
+
+
+
+def create_model_inp(model_id,interval,seismic_data,project_model_path):
 	'''change model_sample.inp into modelXXX.inp file, the main difference is the seismic data part.
 	input:
 	 - idx: int, the id of this model
@@ -21,13 +25,14 @@ def create_model_inp(idx,seismic_data,addr = "Models"):
 	  None
 	'''
 
-	model_path = os.path.join(addr,"model"+str(idx).zfill(3)+'.inp')
+	model_path = project_model_path / "model{}.inp".format(str(model_id).zfill(3))
+	template_path = project_model_path / "model_sample.inp"
 
 	# read the template file model_sample.inp
-	with open(os.path.join(addr,"model_sample.inp"),'r') as f:
+	with open(template_path,'r') as f:
 		data = f.readlines()
 
-	# split the data into 3 blocks and only change the middle one
+	# split the data into 3 blocks 
 	start_content_id,end_content_id = -1,-1
 	for idx,line in enumerate(data):
 		if "*Amplitude" in line:
@@ -37,15 +42,34 @@ def create_model_inp(idx,seismic_data,addr = "Models"):
 			end_content_id = idx
 			break
 	data_prev = data[:start_content_id+1]
-	data_next = data[end_content_id:]
 	data_process = ""
+	data_next = data[end_content_id:]
+	
 
-	# generate the middle part from seismic_data
+	# change job name and model name in the first part
+	for idx,line in enumerate(data_prev):
+		if "JOB" in line or "Job" in line or "job" in line:
+			newline = line.split()
+			newline[3] = 'job'+ str(model_id).zfill(3)
+			newline[6] = 'model'+ str(model_id).zfill(3)
+			newline = ' '.join(newline) +'\n'
+			data_prev[idx] = newline 
+			break
+
+	# generate the second part from seismic_data
 	for idx,(x,y) in enumerate(seismic_data):
 		if idx%4 ==3:
 			data_process+="        {:.3f},        {:f}\n".format(x,y)
 		else:
 			data_process+="        {:.3f},        {:f},".format(x,y)
+
+	# modify time interval and total time of step
+	for idx,line in enumerate(data_next):
+		if "*Dynamic" in line:
+			timeId = idx +1
+			break
+	total_time = max([x for x,_ in seismic_data])
+	data_next[timeId] = "{},{}\n".format(interval,total_time)
 
 	# output the modelXXX.inp
 	output = "".join(data_prev)+data_process+"".join(data_next)
@@ -78,86 +102,68 @@ def extract_seismic_data(filepath):
 	res = []
 	for i in range(len(amplitude)):
 		res.append(((i+1)*deltaT,amplitude[i]))
-	return res
+	return deltaT,res
 
 
 
 def main():
+	# basic informations
+	projectNames = ['ThreeLayersFrame','FiveLayersFrame']
+	CUR_PATH = Path(__file__).absolute().parent
+	RESULT_PATH = CUR_PATH / "Result"
+	MODEL_PATH = CUR_PATH / "Models"
+	DATA_PATH = CUR_PATH / "Data"
 
-	## init for id_wavename_map
-	id_wavename_map_path = 'Result/id_wavename_map.pkl'
-	if os.path.isfile(id_wavename_map_path):
-		id_wavename_map = load_pkl(id_wavename_map_path)
-	else:
-		id_wavename_map = {}
+	for projectName in projectNames:
+		project_result_path = RESULT_PATH / projectName
+		project_model_path = MODEL_PATH / projectName
 
-	wavename_map_list = [v for _,v in id_wavename_map.items()]
-	start_id = len(wavename_map_list)
-	wavename_id_map = {wavename:idx for idx,wavename in id_wavename_map.items()}
+		if not project_model_path.is_dir():
+			raise ValueError("Please check whether {} is in file Models".format(projectName))
+		if not project_result_path.is_dir():
+			project_result_path.mkdir()
 
-	## init for id_data_map
-	id_data_map_path = 'Result/id_data_map.pkl'
-	if os.path.isfile(id_data_map_path):
-		id_data_map = load_pkl(id_data_map_path)
-	else:
-		id_data_map = {}
+		## init for id_wavename_map
+		id_wavename_map_path = project_result_path / 'id_wavename_map.pkl'
+		if id_wavename_map_path.is_file():
+			id_wavename_map = load_pkl(id_wavename_map_path)
+		else:
+			id_wavename_map = {}
 
-	## find all proper files and process
-	for root,dirs,files in os.walk("Data"):
-		for filename in files:
-			if ".AT2" in filename and "UP" not in filename:
-				wavename = root.split('\\')[-1]+"@"+filename
+		wavename_map_list = [v for _,v in id_wavename_map.items()]
+		current_id = len(wavename_map_list)
+		wavename_id_map = {wavename:idx for idx,wavename in id_wavename_map.items()}
 
-				## id_wave_map process
-				if wavename not in wavename_map_list:
-					id_wavename_map[start_id] = wavename
-					wavename_id_map[wavename] = start_id
-					start_id+=1
+		## init for id_data_map
+		id_data_map_path = project_result_path / 'id_data_map.pkl'
+		if id_data_map_path.is_file():
+			id_data_map = load_pkl(id_data_map_path)
+		else:
+			id_data_map = {}
 
-				## id_data_map process
-				idx = wavename_id_map[wavename]
-				if idx not in id_data_map:
-					seismic_data = extract_seismic_data(os.path.join(root,filename))
-					create_model_inp(idx,seismic_data)
-					id_data_map[idx] = seismic_data
-	## save data
-	save_pkl(id_wavename_map,id_wavename_map_path)
-	save_pkl(id_data_map,id_data_map_path)
+		## find all proper files and process
+		for root,dirs,files in os.walk(DATA_PATH):
+			for filename in files:
+				if ".AT2" in filename and "UP" not in filename:
+					root_path = Path(root)
+					wavename = root_path.name+"@"+filename
 
+					## id_wave_map process
+					if wavename not in wavename_map_list:
+						id_wavename_map[current_id] = wavename
+						wavename_id_map[wavename] = current_id
+						current_id+=1
 
+					## id_data_map process
+					idx = wavename_id_map[wavename]
+					if idx not in id_data_map:
+						interval,seismic_data = extract_seismic_data(root_path / filename)
+						id_data_map[idx] = seismic_data
+						create_model_inp(idx,interval,seismic_data,project_model_path)
+		## save data
+		save_pkl(id_wavename_map,id_wavename_map_path)
+		save_pkl(id_data_map,id_data_map_path)
 
-def play_around():
-	with open('Models/model_sample.inp','r') as f:
-		data = f.readlines()
-	start_content_id,end_content_id = -1,-1
-	for idx,line in enumerate(data):
-		if "*Amplitude" in line:
-			start_content_id = idx
-			continue
-		if start_content_id != -1 and line[0] == '*':
-			end_content_id = idx
-			break
-	data_prev = data[:start_content_id+1]
-	data_next = data[end_content_id:]
-	data_procecss = [line.split() for line in data[start_content_id+1:end_content_id]]
-	for line in data[start_content_id+1:end_content_id]:
-		print(line)
-
-def get_deltaT():
-	data = load_pkl("Result/id_data_map.pkl")
-	for idx,value in data.items():
-		t_list = [t for t,A in value]
-		t_list.sort()
-		print(idx,round(t_list[1] - t_list[0],3))
-def test():
-		## Check exist res_dict
-	res_path = "Result"
-	data = load_pkl("Result/res_dict.pkl")
-	for idx,(k,v) in enumerate(data.items()):
-		print(v)
-		break
 
 if __name__ == '__main__':
-	test()
-	# get_deltaT()
-	# play_around()
+	main()
